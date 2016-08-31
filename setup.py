17b844b488afd2050caca7d69f0e629f3543572d
Manesh5491 <maneshmnair3@gmail.com>
2016-08-31 12:38:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author_email = '',
     description = 'Automation Library',
     long_description='Api for automation',
-    url = 'https://github.azc.ext.hp.com/manesh-nair/TestRepo', # The URL to the github repo
-    download_url = 'https://github.azc.ext.hp.com/manesh-nair/TestRepo.git',
+    url = 'https://github.com/Manesh5491/TestRepo', # The URL to the github repo
+    download_url = 'https://github.com/Manesh5491/TestRepo.git',
     license='MIT',# Choose your license
     classifiers=[
         'Development Status :: 3 - Alpha',
