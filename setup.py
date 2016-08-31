224ba176e64c33a7fca48755559c4b7e0637ac3e
Manesh5491 <maneshmnair3@gmail.com>
2016-08-31 12:37:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = "testRepo",
+    version = "0.1",
+    packages = find_packages(),
+    author = '',
+    author_email = '',
+    description = 'Automation Library',
+    long_description='Api for automation',
+    url = 'https://github.azc.ext.hp.com/manesh-nair/TestRepo', # The URL to the github repo
+    download_url = 'https://github.azc.ext.hp.com/manesh-nair/TestRepo.git',
+    license='MIT',# Choose your license
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. 
+        'Programming Language :: Python :: 2.7'
+        ],
+    install_requires=[
+        "pytest==3.0.1",
+        "Appium-Python-Client==0.22"
+        ],
+    )
